ccea46949491e6a1349445ab42a68f0cb8b1200f
Noah Zinsmeister <noahwz@gmail.com>
2017-03-09 23:25:03 -0500

    Modified listing search params, improved plots
    temporarily uploaded example plots

--- listing_search.R
+++ listing_search.R
@@ -5,19 +5,17 @@ library(magrittr)
 library(ggplot2)
 library(grid)
 library(gridExtra)
+library(tibble)
 
-search_listings = function(location,
-                            guests = 1,
-                            ib = FALSE,
-                            min_bathrooms = 1,
-                            min_bedrooms = 1,
-                            min_beds = 1) {
+search.listings = function(location,
+                           guests = 1,
+                           ib = FALSE) {
     params = list(location = location,
-                  guests = as.character(guests),
-                  ib = tolower(ib),
-                  min_bathrooms = as.character(min_bathrooms),
-                  min_bedrooms = as.character(min_bedrooms),
-                  min_beds = as.character(min_beds),
+                  guests = guests,
+                  ib = ib,
+                  min_bathrooms = 0,
+                  min_bedrooms = 0,
+                  min_beds = 0,
                   client_id = "d306zoyjsyarp7ifhu67rjxn52tv0t20",
                   locale = "en-US",
                   currency = "USD")
@@ -37,44 +35,49 @@ search_listings = function(location,
         bad_request = content(request, as = "parsed")
         stop(paste("\n\nError code: ", bad_request$error_code, "\nError: ", bad_request$error, "\nError Message: ", bad_request$error_message, sep =""))
     }
-    content(request, as = "parsed")
+    c(passed_location = location, content(request, as = "parsed"))
 }
 
-testing_random_stuff = function(c) {
+parse.metadata = function(metadata, passed_location) {
     # geography info
-    country = c$metadata$geography$country
-    state = c$metadata$geography$state
-    city = c$metadata$geography$city
-    recognized_as = paste(c$metadata$geography$result_type, ": ", c$metadata$geography$precision, sep = "")
-    google_center = browseURL(paste("http://maps.google.com/maps?t=m&q=loc:", c$metadata$geography$lat, "+", c$metadata$geography$lng, sep = ""))
+    country = metadata$geography$country
+    state = metadata$geography$state
+    city = metadata$geography$city
+    recognized_as = paste(metadata$geography$result_type, ": ", metadata$geography$precision, sep = "")
+    #google_center = browseURL(paste("http://maps.google.com/maps?t=m&q=loc:", metadata$geography$lat, "+", metadata$geography$lng, sep = ""))
     print(paste(country, city, state, recognized_as, sep = ", "))
 
     #facet info
     facets = list()
     # "at least x" variables
     for (i in c("bedrooms", "bathrooms", "beds")) {
-        facets[[i]] = as.tbl(do.call(rbind, lapply(c$metadata$facets[[i]], data.frame))) %>%
-                               mutate(value = as.numeric(as.character(value))) %>%
-                               select(value, count) %>%
-                               arrange(value) %>%
-                               rename(cumulative_count = count) %>%
-                               mutate(count = cumulative_count - lead(cumulative_count, default = 0))
+        facets[[i]] = lapply(metadata$facets[[i]], lapply, function(x) ifelse(is.null(x), NA, x)) %>%
+                      bind_rows %>%
+                      mutate(value = as.numeric(as.character(value))) %>%
+                      select(value, count) %>%
+                      arrange(value) %>%
+                      rename(cumulative_count = count) %>%
+                      mutate(count = cumulative_count - lead(cumulative_count, default = 0))
     }
     # factor variables
     for (i in c("room_type", "hosting_amenity_ids", "top_amenities")) {
-        facets[[i]] = as.tbl(do.call(rbind, lapply(c$metadata$facets[[i]], data.frame))) %>%
+        facets[[i]] = lapply(metadata$facets[[i]], lapply, function(x) ifelse(is.null(x), 0, x)) %>%
+            bind_rows %>%
             mutate(value = as.character(value)) %>%
             select(value, count) %>%
             arrange(desc(count))
     }
-    # messy plotting, colors don't seem to be working?
-    p1 = ggplot(facets$bathrooms) + geom_bar(aes(x = value, weight = count, fill = "blue"), show.legend = FALSE) + ggtitle("Bathrooms") + scale_x_continuous(breaks = facets$bathrooms$value)
-    p2 = ggplot(facets$bedrooms) + geom_bar(aes(x = value, weight = count, fill = "#56B4E9"), show.legend = FALSE) + ggtitle("Bedrooms") + scale_x_continuous(breaks = facets$bedrooms$value)
-    p3 = ggplot(facets$beds) + geom_bar(aes(x = value, weight = count, fill = "#6D9DE5"), show.legend = FALSE) + ggtitle("Beds") + scale_x_continuous(breaks = facets$beds$value)
-    p4 = ggplot(facets$room_type) + geom_bar(aes(x = value, weight = count, fill = "#6D9DE5"), show.legend = FALSE) + ggtitle("Room Type") + scale_x_discrete(limits = facets$room_type$value)
-    p5 = ggplot(facets$hosting_amenity_ids) + geom_bar(aes(x = value, weight = count, fill = "#6D9DE5"), show.legend = FALSE) + ggtitle("All Amenities") + scale_x_discrete(limits = facets$hosting_amenity_ids$value) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
-    p6 = ggplot(facets$top_amenities) + geom_bar(aes(x = value, weight = count, fill = "#6D9DE5"), show.legend = FALSE) + ggtitle("Top Amenities") + scale_x_discrete(limits = facets$top_amenities$value)
-    grid.arrange(p1, p2, p3, p4, p5, p6, nrow = 2, ncol = 3, top = textGrob(paste("Listing Characteristics for ", city, ", ", state, sep = ""), gp = gpar(fontsize = 20)))
+    # messy plotting
+    num_listings = sum(facets$bedrooms$count)
+    p1 = ggplot(facets$bathrooms, aes(x = value, weight = count)) + geom_bar(fill = "coral2") + geom_text(aes(y = count, label = scales::percent(round(count/sum(count), 2))), vjust = -.25) + labs(title = "Bathrooms", x = "", y = "Count") + scale_x_continuous(breaks = facets$bathrooms$value)
+    p2 = ggplot(facets$bedrooms, aes(x = value, weight = count)) + geom_bar(fill = "coral2") + geom_text(aes(y = count, label = scales::percent(round(count/sum(count), 2))), vjust = -.25) + labs(title = "Bedrooms", x = "", y = "Count") + scale_x_continuous(breaks = facets$bedrooms$value)
+    p3 = ggplot(facets$beds, aes(x = value, weight = count)) + geom_bar(fill = "coral2") + geom_text(aes(y = count, label = scales::percent(round(count/sum(count), 2))), vjust = -.25) + labs(title = "Beds", x = "", y = "Count") + scale_x_continuous(breaks = facets$beds$value)
+    p4 = ggplot(facets$room_type, aes(x = value, weight = (count/num_listings)*100)) + geom_bar(fill = "skyblue2") + labs(title = "Room Type", x = "", y = "Percent") + scale_x_discrete(limits = facets$room_type$value)
+    p5 = ggplot(facets$top_amenities, aes(x = value, weight = (count/num_listings)*100)) + geom_bar(fill = "skyblue2") + labs(title = "Top Amenities", x = "", y = "Percent") + scale_x_discrete(limits = facets$top_amenities$value)
+    p6 = ggplot(facets$hosting_amenity_ids, aes(x = value, weight = (count/num_listings)*100)) + geom_bar(fill = "skyblue2") + labs(title = "All Amenities", x = "", y = "Percent") + scale_x_discrete(limits = facets$hosting_amenity_ids$value) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .5))
+    p = grid.arrange(p1, p2, p3, p4, p5, p6, nrow = 2, ncol = 3, top = textGrob(paste("Listing Characteristics for ", passed_location, "\n", city, ", ", state, sep = ""), gp = gpar(fontsize = 20)))
+    ggsave(file = paste(passed_location, ".png", sep = ""), p, width = 16, height = 9)
 }
 
-c = search_listings(60615)
+c = search.listings(10019)
+parse.metadata(c$metadata, c$passed_location)
