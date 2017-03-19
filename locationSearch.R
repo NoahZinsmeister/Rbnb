1b1be376033cdaa512283d94523e23d1d2c6334f
Noah Zinsmeister <noahwz@gmail.com>
2017-03-18 22:18:56 -0400

    moved parsing inside searchLocation function

--- locationSearch.R
+++ locationSearch.R
@@ -7,10 +7,6 @@ library(grid)
 library(gridExtra)
 library(tibble)
 
-# write a metadata thing with just metadata
-# graphing metadata function
-#
-
 constructGET = function(base.url, parameters) {
     # first, replace all spaces with %20
     parameters = lapply(parameters, function(x) gsub(" ", "%20", x))
@@ -31,6 +27,7 @@ checkRequest = function(request) {
 
 searchLocation = function(location,
                           verbose = TRUE,
+                          metadata.only = FALSE,
                           client.id = "d306zoyjsyarp7ifhu67rjxn52tv0t20") {
     # default search parameters
     params = list(location = location,
@@ -45,7 +42,7 @@ searchLocation = function(location,
                   price_min = 0,
                   price_max = 2000,
                   sort = 1,
-                  "_format" = "for_search_results",
+                  "_format" = "for_search_results_with_minimal_pricing",
                   "_limit" = 1,
                   "_offset" = 0)
     endpoint.url = "https://api.airbnb.com/v2/search_results"
@@ -62,10 +59,14 @@ searchLocation = function(location,
     if (num.listings == 0)
         stop("No results found. Try a different search term.")
 
+    if (metadata.only)
+        return(list(passed.location = location,
+                    metadata = parseMetadata(primary.results$metadata)))
+
     # else, figure out how many max prices cutoffs we need to get x or fewer listings between cutoffs
     # NOTE: this is because if num.listings is over 1000, it means that there are more results
     # than we're able to get by manipulating limit and offset, hence using price cutoffs
-    get.num.listings = function(p.low, p.high) {
+    getNumListings = function(p.low, p.high) {
         # gets number of listings in [p.low, p.high-1]
         params$price_min = p.low
         params$price_max = p.high-1
@@ -79,7 +80,7 @@ searchLocation = function(location,
     # starting cutoffs [0-2000]
     price.cutoffs = c(0, 2001)
 
-    # make cutoffs more granular until we get the proper amount
+    # make cutoffs more granular until we have bins of <250 listings
     while (any(num.listings > 250)) {
         # between which cutoffs are there most listings?
         max.index = which.max(num.listings)
@@ -90,8 +91,8 @@ searchLocation = function(location,
         # insert new cutoff
         price.cutoffs = append(price.cutoffs, new.cutoff, after = max.index)
         # add in number of listings for new cutoffs
-        num.listings = append(num.listings, c(get.num.listings(price.cutoffs[max.index], price.cutoffs[max.index+1]),
-                                              get.num.listings(price.cutoffs[max.index+1], price.cutoffs[max.index+2])),
+        num.listings = append(num.listings, c(getNumListings(price.cutoffs[max.index], price.cutoffs[max.index+1]),
+                                              getNumListings(price.cutoffs[max.index+1], price.cutoffs[max.index+2])),
                               after = max.index)
         # and remove the old cutoff
         num.listings = num.listings[-max.index]
@@ -113,43 +114,41 @@ searchLocation = function(location,
         params$price_min = price.cutoffs[i]
         params$price_max = price.cutoffs[i+1]-1
 
-        # check how many calls we need to make
-        finished.iterations = FALSE
-
         # make the necessary calls, saving results only (not metadata) at each step
-        while (!finished.iterations) {
+        repeat {
             request = RETRY("GET", url = constructGET(endpoint.url, params))
             checkRequest(request)
             parsed.results = content(request, as = "parsed")
             all.results = c(all.results, parsed.results$search_results)
             params$`_offset` = parsed.results$metadata$pagination$next_offset
-            if (parsed.results$metadata$pagination$result_count < 50) finished.iterations = TRUE
+            if (parsed.results$metadata$pagination$result_count < 50) break
         }
         params$`_offset` = 0
     }
 
     # return the list of all results, metadata from last call, and passed location
     list(passed.location = location,
-         metadata = primary.results$metadata,
-         results = all.results)
+         metadata = parseMetadata(primary.results$metadata),
+         results = list(num.listings = length(all.results),
+                        data = parseResults(all.results)))
 }
 
-parseMetadata = function(content) {
+parseMetadata = function(metadata) {
     # geography info
-    geography = list(city = content$metadata$geography$city,
-                     state = content$metadata$geography$state,
-                     country = content$metadata$geography$country,
-                     result.type = content$metadata$geography$result.type,
-                     precision = content$metadata$geography$precision,
-                     lat = content$metadata$geography$lat,
-                     lng = content$metadata$geography$lng)
+    geography = list(city = metadata$geography$city,
+                     state = metadata$geography$state,
+                     country = metadata$geography$country,
+                     result.type = metadata$geography$result.type,
+                     precision = metadata$geography$precision,
+                     lat = metadata$geography$lat,
+                     lng = metadata$geography$lng)
     geography$google.maps.loc = paste0("http://maps.google.com/maps?t=m&q=loc:",
                                        geography$lat, "+", geography$lng)
     # facet info
     facets = list()
     # "at least x" variables
     for (i in c("bedrooms", "bathrooms", "beds")) {
-        facets[[i]] = lapply(content$metadata$facets[[i]], lapply,
+        facets[[i]] = lapply(metadata$facets[[i]], lapply,
                              function(x) ifelse(is.null(x), 0, x)) %>%
                       bind_rows %>%
                       mutate(value = as.numeric(as.character(value))) %>%
@@ -159,42 +158,41 @@ parseMetadata = function(content) {
                       mutate(count = cumulative.count - lead(cumulative.count,
                                                              default = 0))
     }
+    num.listings = facets$bedrooms$cumulative.count[1]
     # factor variables
     for (i in c("room_type", "hosting_amenity_ids", "top_amenities",
                 "languages")) {
-        facets[[i]] = lapply(content$metadata$facets[[i]], lapply,
+        facets[[i]] = lapply(metadata$facets[[i]], lapply,
                              function(x) ifelse(is.null(x), 0, x)) %>%
             bind_rows %>%
             mutate(value = as.character(value)) %>%
             select(value, count) %>%
             arrange(desc(count))
     }
-    list(passed.location = content$passed.location,
-         num.listings = content$num.listings,
+    list(num.listings = num.listings,
          geography = geography,
          facets = facets)
 }
 
-parseResults = function(content) {
-    bind_rows(lapply(content$results,
-                     function(x) bind_rows(as.list(unlist(x)))))
+parseResults = function(results) {
+    tbl = bind_rows(lapply(results, function(x) bind_rows(as.list(unlist(x)))))
+
+    # to do...
+    rename.vars = c("listing.name" = "listing.name", "listing.id" = "id", "listing.bedrooms" = "bedrooms",
+                    "listing.beds" = "beds", "listing.bathrooms" = "bathrooms", "listing.person.capacity" = "capacity",
+                    "listing.city" = "city", "listing.public.address" = "public.address",
+                    "listing.neighborhood" = "neighborhood", "listing.lat" = "lat", "listing.lng" = "lng",
+                    "listing.property.type" = "property.type",
+                    "listing.room.type" = "room.type", "listing.star.rating" = "rating",
+                    "listing.primary.host.first.name" = "host.name", "listing.primary.host.id" = "host.id",
+                    "listing.instant.bookable" = "instant.book",
+                    "listing.is.business.travel.ready" = "business.travel.ready", "listing.picture.count" = "num.pictures",
+                    "listing.reviews.count" = "num.reviews", "pricing.quote.guests" = "price.quote.num.guests",
+                    "pricing.quote.localized.currency" = "price.quote.currency", "pricing.quote.localized.nightly.price" = "price.quote.price",
+                    "pricing.quote.localized.service.fee" = "price.quote.fee")
+    tbl
 }
+
 
 location = "10019"
 content = searchLocation(location)
-
-rename.vars = c("listing.name" = "listing.name", "listing.id" = "id", "listing.bedrooms" = "bedrooms",
-                "listing.beds" = "beds", "listing.bathrooms" = "bathrooms", "listing.person.capacity" = "capacity",
-                "listing.city" = "city", "listing.public.address" = "public.address",
-                "listing.lat" = "lat", "listing.lng" = "lng", "listing.property.type" = "property.type",
-                "listing.room.type" = "room.type", "listing.star.rating" = "rating",
-                "listing.primary.host.first.name" = "host.name", "listing.primary.host.id" = "host.id",
-                "listing.instant.bookable" = "instant.book",
-                "listing.is.business.travel.ready" = "business.travel.ready", "listing.picture.count" = "num.pictures",
-                "listing.reviews.count" = "num.reviews", "pricing.quote.guests" = "price.quote.num.guests",
-                "pricing.quote.localized.currency" = "price.quote.currency", "pricing.quote.localized.nightly.price" = "price.quote.price",
-                "pricing.quote.localized.service.fee" = "price.quote.fee")
-# want to put this in but not defined for all places..."listing.neighborhood" = "neighborhood"
-
-result = parseResults(content)
-metadata = parseMetadata(content)
