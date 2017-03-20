d2d6e27cffb853180ca0969c88860d5670a1be83
Alex Kroeger <alexrkroeger@gmail.com>
2017-03-19 23:26:08 -0500

    Added documentation for data files.

---
+++ R/listing.detail.R
@@ -0,0 +1,8 @@
+#' Detailed listing data from \code{\link{listingDetails}("12170773")}
+#'
+#' Description.
+#' @name listing.detail
+#' @docType data
+#' @keywords data
+#'
+NULL
---
+++ R/trainData.R
@@ -0,0 +1,9 @@
+#' Listing data from \code{searchLocation} with details merged in with 
+#' \code{addDetails} for use in predicting a price for Listing \code{12170773}.
+#'
+#' Description.
+#' @name trainData
+#' @docType data
+#' @keywords data
+#'
+NULL
