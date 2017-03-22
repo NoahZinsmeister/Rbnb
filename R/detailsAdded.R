f5a5932ce15ea13816ca2b16ab13312f40dc5208
Alex Kroeger <alexrkroeger@gmail.com>
2017-03-21 20:34:24 -0500

    Added a documentation file for the detailsAdded dataset.

---
+++ R/detailsAdded.R
@@ -0,0 +1,9 @@
+#' Listing data from \code{searchLocation} with more detailed 
+#' information merged in using \code{addDetails}.
+#'
+#' Description.
+#' @name detailsAdded
+#' @docType data
+#' @keywords data
+#'
+NULL
